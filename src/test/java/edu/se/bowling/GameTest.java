d58c579a5422b748b4207bba65cc41ea1e49644f
Testing <test@gmail.com>
2026-04-18 16:50:28 +0200

    test: all ones scores twenty

--- src/test/java/edu/se/bowling/GameTest.java
+++ src/test/java/edu/se/bowling/GameTest.java
@@ -6,14 +6,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GameTest {
 
     @Test
-
     void gutterGameScoresZero(){
         Game game = new Game();
 
         for (int i = 0; i < 20; i++){
-            game.roll();
+            game.roll(0);
         }
 
         assertEquals(0, game.score());
+    }
+
+    @Test
+    void allOnesScoresTwenty(){
+        Game game = new Game();
+
+        for (int i = 0; i < 20; i++){
+            game.roll(1);
+        }
+
+        assertEquals(20, game.score());
     }
 }
