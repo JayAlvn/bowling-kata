2173a614c6b4500007c1d9bb1c3892a9a1688f94
Testing <test@gmail.com>
2026-04-18 16:42:44 +0200

    feat: implement roll and score for the gutter game

--- src/main/java/edu/se/bowling/Game.java
+++ src/main/java/edu/se/bowling/Game.java
@@ -3,8 +3,22 @@ package edu.se.bowling;
 
 public class Game {
 
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
-    public static void main(String[] args) {
 
+    public void roll(int pins){
+        rolls[currentRoll++] = pins;
     }
+
+    public int score(){
+        int score = 0;
+        for (int i = 0; i < 20; i++){
+            score += rolls[i];
+        }
+
+        return score;
+    }
+
+
 }
